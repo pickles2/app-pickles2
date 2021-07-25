896bb8de41355499549125f5acb998af7333468a
Tomoya Koyanagi <tomk79@gmail.com>
2021-07-25 19:04:10 +0900

    <SettingConditionChecker> 起動時に必要なコマンドが利用できない状態の場合、設定ダイアログを表示するようになった。

--- app/index_files/settingConditionChecker.js
+++ app/index_files/settingConditionChecker.js
@@ -46,6 +46,10 @@ module.exports = function(main, window){
 		]);
 	}
 
+
+	/**
+	 * コマンドが利用可能かチェックする
+	 */
 	function checkCmd(cmd, callback){
 		var res = {
 			"available": false,
@@ -59,14 +63,15 @@ module.exports = function(main, window){
 		if( cmd == 'git' ){
 			subCmd = ['--version'];
 		}
+		var targetCmd = cmd;
 
 		if( main.px2dtLDA.db.commands && main.px2dtLDA.db.commands[cmd] ){
-			cmd = main.px2dtLDA.db.commands[cmd];
+			targetCmd = main.px2dtLDA.db.commands[cmd];
 		}
-		console.log('--- cmd:', cmd + ' ' + subCmd.join(' '));
+		console.log('--- cmd:', targetCmd + ' ' + subCmd.join(' '));
 
 		window.px.utils.spawn(
-			cmd,
+			targetCmd,
 			subCmd,
 			{
 				success: function(data){
@@ -81,12 +86,57 @@ module.exports = function(main, window){
 				complete: function(code){
 					// console.log(code);
 					res.code = code;
+					if( !res.available || res.code !== 0 ){
+						cmdSettingDialog(cmd, function(){
+							checkCmd(cmd, callback);
+							return;
+						});
+						return;
+					}
 					callback(res);
 				}
 			}
 		);
 
 		return;
+	}
+
+
+	/**
+	 * コマンドが利用できない状態の場合に、設定画面を表示する
+	 */
+	function cmdSettingDialog( cmd, callback ){
+		var targetCmd = cmd;
+		if( main.px2dtLDA.db.commands && main.px2dtLDA.db.commands[cmd] ){
+			targetCmd = main.px2dtLDA.db.commands[cmd];
+		}
+
+		var $body = $('<div>');
+		$body
+			.append( $('<p>').text('ようこそ！') )
+			.append( $('<p>').text('このアプリケーションでは、 ' + cmd + ' コマンドを利用します。') )
+			.append( $('<p>').text('現在、このコマンドは利用できる準備が整っていません。 ' + cmd + ' コマンドの有効なパスを設定してください。') )
+			.append( $('<p>').text('お使いの環境に '+cmd+' コマンドがインストールされていない場合は、先にインストールしてください。') )
+			.append( $('<p>')
+				.append( $('<input type="text" name="cmd-new-path" value="" class="px2-input px2-input--block" />').val(targetCmd) )
+			)
+		;
+
+		px2style.modal({
+			'title': cmd + ' コマンドのパス設定',
+			'body': $body,
+			'buttons':[
+				$('<button class="px2-btn px2-btn--primary">')
+					.text('OK')
+					.on('click', function(){
+						var newCmdPath = $body.find('input[name=cmd-new-path]').val();
+						main.px2dtLDA.db.commands[cmd] = newCmdPath;
+
+						px2style.closeModal();
+						callback();
+					})
+			],
+		});
 	}
 
 }
